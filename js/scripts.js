ca1190a21af90bc15f58e15c408b525162c527fc
Przemek Saja <przemek.saja@gmail.com>
2018-05-12 22:52:37 +0200

    Add scrrpits.js file

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+var a = prompt('Enter some value'),
+	h = 3,
+	triangleArena = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArena);
